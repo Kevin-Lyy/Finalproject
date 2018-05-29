c702be9cf9297375ef0b2b25662ef2aff357b5a2
unknown <batmanthelegendly@gmail.com>
2018-05-29 00:06:10 -0400

    works as far as i know

--- TicTacToeRedux.java
+++ TicTacToeRedux.java
@@ -57,41 +57,6 @@ public class TicTacToeRedux{
     return reString;
   }
 
-  public static int checkBoard2(int[][] board){
-    if(board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] == board[0][2]){
-      if(board[0][0] == 1) return 1;
-      else if(board[0][0] == 2) return 2;
-    }
-    if(board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] == board[2][0]){
-      if(board[0][0] == 1) return 1;
-      else if(board[0][0] == 2) return 2;
-    }
-    if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] == board[2][2]){
-      if(board[0][0] == 1) return 1;
-      else if(board[0][0] == 2) return 2;
-    }
-    if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] == board[2][0]){
-      if(board[0][2] == 1) return 1;
-      else if(board[0][0] == 2) return 2;
-    }
-    if(board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] == board[1][2]){
-      if(board[0][2] == 1) return 1;
-      else if(board[0][0] == 2) return 2;
-    }
-    if(board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] == board[1][2]){
-      if(board[1][0] == 1) return 1;
-      else if(board[0][0] == 2) return 2;
-    }
-    if(board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] == board[2][2]){
-      if(board[2][0] == 1) return 1;
-      else if(board[0][0] == 2) return 2;
-    }
-    if(board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[1][1] == board[2][1]){
-      if(board[0][0] == 1) return 1;
-      else if(board[0][0] == 2) return 2;
-    }
-    return 0;
-  }
   public static int checkBoard(int[][] board){
     if(board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] == board[0][2] && board[0][0] == 1) return 1;
     if(board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] == board[0][2] && board[0][0] == 2) return 2;
@@ -106,16 +71,16 @@ public class TicTacToeRedux{
     if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] == board[2][0] && board[0][2] == 2) return 2;
 
     if(board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] == board[1][2] && board[0][2] == 1) return 1;
-    if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] == board[2][0] && board[0][2] == 2) return 2;
+    if(board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] == board[1][2] && board[0][2] == 2) return 2;
 
     if(board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] == board[1][2] && board[1][0] == 1) return 1;
-    if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] == board[2][0] && board[1][0] == 2) return 2;
+    if(board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] == board[1][2] && board[1][0] == 2) return 2;
 
     if(board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] == board[2][2] && board[2][0] == 1) return 1;
-    if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] == board[2][0] && board[2][0] == 2) return 2;
+    if(board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] == board[2][2] && board[2][0] == 2) return 2;
 
     if(board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[1][1] == board[2][1] && board[0][1] == 1) return 1;
-    if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] == board[2][0] && board[0][0] == 2) return 2;
+    if(board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[1][1] == board[2][1] && board[0][1] == 2) return 2;
 
     return 0;
   }
@@ -130,6 +95,18 @@ public class TicTacToeRedux{
     if(x == 0 && y == 2) nextBoard = b7;
     if(x == 1 && y == 2) nextBoard = b8;
     if(x == 2 && y == 2) nextBoard = b9;
+  }
+  public static String printNextTurn(int x, int y){
+    if(x == 0 && y == 0) return "b1";
+    if(x == 1 && y == 0) return "b2";
+    if(x == 2 && y == 0) return "b3";
+    if(x == 0 && y == 1) return "b4";
+    if(x == 1 && y == 1) return "b5";
+    if(x == 2 && y == 1) return "b6";
+    if(x == 0 && y == 2) return "b7";
+    if(x == 1 && y == 2) return "b8";
+    if(x == 2 && y == 2) return "b9";
+    return "how the fuck did this happen";
   }
 
   public static void setLargeBoard(){
@@ -180,24 +157,32 @@ public class TicTacToeRedux{
     System.out.println(toStringLarge(lrgBoard));
     System.out.println("\n");
     System.out.println("O goes first, input your place");
+    System.out.println("input the game number first, press enter and begin the first move");
 
     while(checkBoard(lrgBoard) == 0){
       System.out.println("input: ");
       int j;
       int k;
+      String playernames;
+
       try{
+        playernames = scanner.nextLine();
+
         j = scanner.nextInt();
         k = scanner.nextInt();
         if(j < 0 || k < 0 || j > 2 || k > 2){
-          System.out.println("invalid input");
+          System.out.println("Try a number less than 2 but greater than 0");
           continue;
         }
       }catch (InputMismatchException e) {
-				System.out.println("no");
+				System.out.println("number please");
 				continue;
       }
       play(k,j);
+
       System.out.println(toStringLarge(lrgBoard));
+      System.out.println(printNextTurn(j,k));
+      /*
       System.out.println("b1 " + checkBoard(b1));
       System.out.println("b2 " + checkBoard(b2));
       System.out.println("b3 " + checkBoard(b3));
@@ -207,9 +192,9 @@ public class TicTacToeRedux{
       System.out.println("b7 " + checkBoard(b7));
       System.out.println("b8 " + checkBoard(b8));
       System.out.println("b9 " + checkBoard(b9));
+      */
+      if(checkBoard(lrgBoard) == 1) System.out.println("the winner is player1" );
+      else if(checkBoard(lrgBoard) == 2)System.out.println("the winner is player2" );
     }
-
-    if(checkBoard(lrgBoard) == 1) System.out.println("the winner is player 1");
-    else if(checkBoard(lrgBoard) == 2)System.out.println("the winner is player 2");
   }
 }
